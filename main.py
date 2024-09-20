1266de0e5a2fa296d748729ada77c7dd98547b5e
oraculodata <oraculodata@gmail.com>
2024-09-20 21:35:38 +0200

    New updates

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ st.title("Crimes in London Analysis")
 st.write("""
 Welcome to the London Crimes Dashboard. Use the sidebar to navigate through different pages and filter the data.
 """)
+
+# Centralizar a bandeira
+st.image("london_flag.jpg", use_column_width=True)
+
+# Texto com o hyperlink
+st.markdown("""
+The data has been extracted from [https://data.police.uk/about](https://data.police.uk/about), so they are real data.
+""")
