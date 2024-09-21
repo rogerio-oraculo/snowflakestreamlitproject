f87c2800ec3b81f4470fbc5da0e54356094d2535
rogerioelquinto <rogerioelquinto@gmail.com>
2024-09-21 22:46:52 +0200

    🟢

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ st.write("""
 Welcome to the London Crimes Dashboard. Use the sidebar to navigate through different pages and filter the data.
 """)
 
-# Centralizar a bandeira
 st.image("london_flag.jpg", use_column_width=True)
 
-# Texto com o hyperlink
 st.markdown("""
 The data has been extracted from [https://data.police.uk/about](https://data.police.uk/about), so they are real data.
 """)
